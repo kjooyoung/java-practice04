7aabba4c55626d298f5337ed54e9d08bd06370da
jooyoung <jooyoung@DESKTOP-KUP8SIT>
2019-01-04 09:27:48 +0900

    finished

--- src/prob01/Printer.java
+++ src/prob01/Printer.java
@@ -2,19 +2,39 @@ package prob01;
 
 public class Printer {
 	
-	public void println(int i) {
-		System.out.println(i);
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	
+//	public void println(String s) {
+//		System.out.println(s);
+//	}
+//	
+//	public void println(boolean b) {
+//		System.out.println(b);
+//	}
+//	
+//	public void println(double d) {
+//		System.out.println(d);
+//	}
+	
+	public <T> void println(T t) {
+		System.out.println(t);
 	}
 	
-	public void println(String s) {
-		System.out.println(s);
+	//가변 변수 (파라미터 값의 개수를 정하지 않고 다 받을 때)
+	public int sum(Integer... nums) {
+		Integer sum = 0;
+		for(Integer i : nums) {
+			sum += i;
+		}
+		return sum;
 	}
 	
-	public void println(boolean b) {
-		System.out.println(b);
-	}
-	
-	public void println(double d) {
-		System.out.println(d);
+	//제네릭을 사용한 가변 변수
+	public <T> void println(T...ts) {
+		for(T t : ts) {
+			System.out.println(t);
+		}
 	}
 }
